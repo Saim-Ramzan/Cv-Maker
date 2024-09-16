8a608fce5888c120d37a97d9f891529aa69efd4b
Subhan Adeel <ms1640070@gmail.com>
2024-09-17 00:58:51 +0500

    get data firebase

--- src/component/Home/form/EductionForm.jsx
+++ src/component/Home/form/EductionForm.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Button, Form, Input, InputNumber } from 'antd';
+import { Button, Form, Input,DatePicker } from 'antd';
 import { addDoc, collection } from "firebase/firestore"; 
 import { auth, db } from "../../../firebase";
+
 const layout = {
   labelCol: {
     span: 8,
@@ -11,105 +12,97 @@ const layout = {
   },
 };
 
-/* eslint-disable no-template-curly-in-string */
 const validateMessages = {
     required: '${label} is required!',
     types: {
       email: '${label} is not a valid email!',
       number: '${label} is not a valid number!',
     },
-    number: {
-      range: '${label} must be between ${min} and ${max}',
-    },
-  };
-  /* eslint-enable no-template-curly-in-string */
-  
-// Degree(s) Obtained
-// Major/Field of Study
-// Institution Name
-// Graduation Date
-// Relevant Coursework (if applicable)
-// Honors or Awards
+};
 
-  const EducationForm = () => {
-  const onFinish =  (values) => {
-    console.log(values);
+const EducationForm = () => {
+  const { RangePicker } = DatePicker;
+
+  const onFinish = async (values) => {
+    const rangeValue = values['range-picker'];
+
+    const datePicker ={
+      'range-picker': [rangeValue[0].format('YYYY-MM-DD'), rangeValue[1].format('YYYY-MM-DD')],
+    }
+    console.log("value",values);
     
     try {
-      const userData = {
-        personalDetails: {
+      if (!auth.currentUser) {
+        throw new Error("User is not authenticated");
+      }
+      
+      const Education = {
+        educationDetails: {
           name: values.user.name,
           email: values.user.email,
-          age: values.user.age,
-          introduction: values.user.introduction
+          school: values.user.school,
+          website: values.user.website,
+          startDate: values.user.datePicker
         },
       };
-      const docRef =  addDoc(collection(db, auth.currentUser.uid), userData)
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-      })
-      
-      console.log("Document written with ID: ", docRef);
+
+      // Use the correct path for the collection
+      const docRef = await addDoc(collection(db,  auth.currentUser.uid ), Education);
+      console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   };
-    return <div className="flex ">
-         <Form 
-    {...layout}
-    name="nest-messages"
-    onFinish={onFinish}
-    style={{
-      maxWidth: 600,
-    }}
-    validateMessages={validateMessages}
-  >
-    <Form.Item
-      name={['user', 'name']}
-      label="Name"
-      rules={[
-        {
-          required: true,
-        },
-      ]}
-    >
-      <Input />
+
+  return (
+    <div className="flex items-start">
+      <Form 
+        {...layout}
+        layout="vertical"
+        name="nest-messages"
+        onFinish={onFinish}
+        style={{ maxWidth: 600 }}
+        validateMessages={validateMessages}
+      >
+        <Form.Item
+          name={['user', 'name']}
+          label="Name"
+          rules={[{ required: true }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={['user', 'email']}
+          label="Email"
+          rules={[{ type: 'email' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="range-picker" label="StartDate" >
+      <RangePicker />
     </Form.Item>
-    <Form.Item
-      name={['user', 'email']}
-      label="Email"
-      rules={[
-        {
-          type: 'email',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
-    <Form.Item
-      name={['user', 'school']}
-      label="School"
-    >
-      <Input />
-    </Form.Item>
-    <Form.Item name={['user', 'website']} label="Website">
-      <Input />
-    </Form.Item>
-    <Form.Item name={['user', 'introduction']} label="Introduction">
-      <Input.TextArea />
-    </Form.Item>
-    <Form.Item
-      wrapperCol={{
-        ...layout.wrapperCol,
-        offset: 8,
-      }}
-    >
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-    </div>;
+        <Form.Item
+          name={['user', 'school']}
+          label="School"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name={['user', 'website']}
+          label="Website"
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{ ...layout.wrapperCol, offset: 8 }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 };
 
 export default EducationForm;
